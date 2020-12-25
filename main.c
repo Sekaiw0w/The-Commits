a8cae4ba096b723b93557ad2d99224b09db6baa4
unknown <Sekai@DESKTOP-29394.29394>
2020-12-24 20:02:37 -0500

    First Commit

---
+++ main.c
@@ -0,0 +1,25 @@
+
+
+/*  Programmer  : Sekai Smart-Macaulay
+    Date        : 9/10/2018
+    FileName    : PrintingSum
+    Purpose     : To find the sum of two numbers
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float num1, num2, sum=0; // Declares the variables
+
+    printf("Enter two numbers \n"); // Prompts the user to enter two numbers
+    scanf("%f", &num1); // Reads the variables
+    scanf("%f", &num2); // Read the variables
+
+    sum=num1+num2; // Calculates the sum
+
+    printf("Sum is %i", sum); // Displays the sum
+
+    return 0;
+}
